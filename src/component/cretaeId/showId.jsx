55c7bc65e841cc257453360de7ba7cb591a00389
punya <punyachapagain@gmail.com>
2019-03-25 18:09:43 +1100

    add address to ui

--- src/component/cretaeId/showId.jsx
+++ src/component/cretaeId/showId.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 
 
 const ShowId = ({ value }) => {
+    const [NSW, QLD, VIC, WA] = ["New South Wales", "Queensland", "Victoria", "Western Australia"]
+    let st = value.state
+    console.log(NSW)
     return (
         <div id="show-id">
             <div className="top">
                 <span className="head-span">Driver Liscense</span>
 
-                <span className="address-span">{value.state}, Australia</span>
+
+                <span className="top-span">{st}, Australia</span>
             </div>
-            <div class="main-show">
-                <div class="left-side">
+            <div className="main-show">
+                <div className="left-side">
                     {value.name}
+                    <span className="span-address">
+                        <span>{value.unitNo}</span>
+                        <span>{value.address}</span>
+                        <span>{value.city} {value.state}</span>
+
+                    </span>
                 </div>
                 <div calss="right-side">
-                    638982938298
+
                 </div>
 
             </div>
