9f38d9140062f35cbef3cdce7c6833bad29ac3ee
punya <punyachapagain@gmail.com>
2019-03-26 17:58:51 +1100

    add disable functionality to select

--- src/component/cretaeId/createId.jsx
+++ src/component/cretaeId/createId.jsx
@@ -8,24 +8,57 @@ class CreateId extends Component {
 
     state = {
         detail: {
-            cardNo: "",
-            liscenseNo: "",
+            // cardNo: "",
+            // liscenseNo: "",
             name: "",
             unitNo: "",
             address: "",
             city: "",
-            state: "",
             postCode: "",
             dateOfBirth: "",
             expiryDate: "",
+            disable: false,
+            disable1: false
+
         }
 
     }
 
+
+
     handleChange = e => {
         const detail = { ...this.state.detail }
         detail[e.currentTarget.name] = e.currentTarget.value
+
+        this.handleOptionChange(e, detail)
         this.setState({ detail })
+
+
+    }
+
+    handleOptionChange = (e, obj) => {
+        if ((e.currentTarget.name === "liscense") && (e.currentTarget.value === "P")) {
+            obj.disable = true;
+            obj.disable1 = true;
+            obj.expiryDate = "How many Year"
+
+        }
+        else if ((e.currentTarget.name === "liscense") && (e.currentTarget.value === "P2")) {
+
+            obj.disable = true;
+            obj.disable1 = false;
+            obj.expiryDate = "How many Year"
+
+        }
+        else if ((e.currentTarget.name === "liscense") && (e.currentTarget.value === "Full")) {
+            obj.expiryDate = "How many Year"
+        }
+        else {
+            obj.disable = false;
+            obj.disable1 = false;
+
+        }
+
 
     }
 
--- src/component/cretaeId/form.jsx
+++ src/component/cretaeId/form.jsx
@@ -2,61 +2,79 @@ import React from 'react';
 
 
 const DetailForm = ({ onChange, value }) => {
-    const { name, unitNo, address, city, postCode } = value
-    return (<div>
-        <form>
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input id="name" type="text" className="form-control" autoFocus
-                    onChange={onChange} value={name} name="name" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="unitNo">Unit No.</label>
-                <input id="unitNo" type="text" className="form-control"
-                    onChange={onChange} value={unitNo} name="unitNo" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="address">Address</label>
-                <input id="address" type="text" className="form-control"
-                    onChange={onChange} value={address} name="address" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="city">City</label>
-                <input id="city" type="text" className="form-control"
-                    onChange={onChange} value={city} name="city" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="state">State</label>
-                <select className="custom-select" id="state" name="state" onChange={onChange}>
-                    <option defaultValue value=""  > Choose..</option>
-                    <option>NSW</option>
-                    <option>QLD</option>
-                    <option>VIC</option>
-                    <option>TAS</option>
-                    <option>WA</option>
-                    <option>SA</option>
-                </select>
-            </div>
-            <div className="form-group">
-                <label htmlFor="postCode">Post Code</label>
-                <input id="postCode" type="text" className="form-control"
-                    onChange={onChange} value={postCode} name="postCode" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="year">Expiry Date</label>
-                <select className="custom-select" id="year" name="expiryDate" onChange={onChange}>
-                    <option defaultValue>Choose..</option>
-                    <option value="1">1 Year</option>
-                    <option value="3">3 Year</option>
-                    <option value="5">5 year</option>
+    const { name, unitNo, address, city, postCode, disable, disable1 } = value
 
-                </select>
-            </div>
-            <button className="btn btn-primary">Submit</button>
 
-        </form>
 
-    </div>);
+    return (
+        <div>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="liscens type">Choose Liscense</label>
+                    <select defaultValue="Liscense Type" className="custom-select" id="Liscense type"
+                        name="liscense" onChange={onChange}>
+                        <option disabled>Liscense Type</option>
+                        <option>P</option>
+                        <option>P2</option>
+                        <option>Full</option>
+
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input id="name" type="text" className="form-control" autoFocus
+                        onChange={onChange} value={name} name="name" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="unitNo">Unit No.</label>
+                    <input id="unitNo" type="text" className="form-control"
+                        onChange={onChange} value={unitNo} name="unitNo" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="address">Address</label>
+                    <input id="address" type="text" className="form-control"
+                        onChange={onChange} value={address} name="address" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="city">City</label>
+                    <input id="city" type="text" className="form-control"
+                        onChange={onChange} value={city} name="city" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="state">State</label>
+                    <select defaultValue="Choose State" className="custom-select" id="state" name="state" onChange={onChange}>
+                        <option disabled>Choose State</option>
+                        <option>NSW</option>
+                        <option>QLD</option>
+                        <option>VIC</option>
+                        <option>TAS</option>
+                        <option>WA</option>
+                        <option>SA</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="postCode">Post Code</label>
+                    <input id="postCode" type="text" className="form-control"
+                        onChange={onChange} value={postCode} name="postCode" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="year">Expiry Date</label>
+                    <select defaultValue="How many Year" className="custom-select" id="year" name="expiryDate" onChange={onChange}>
+                        <option disabled>How many Year</option>
+                        <option >1 Year</option>
+
+                        <option disabled={disable1} >2 Year</option>
+                        <option disabled={disable}>5 year</option>
+
+                    </select>
+                </div>
+
+
+                <button className="btn btn-primary">Submit</button>
+
+            </form>
+
+        </div>);
 }
 
 export default DetailForm;
